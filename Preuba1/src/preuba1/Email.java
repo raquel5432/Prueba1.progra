8f4dcd5bc585c08d79b5f3fb0df38b2f8aaacdba
alira <aliraquel95@gmail.com>
2026-04-20 15:39:19 -0600

    m

--- Preuba1/src/preuba1/Email.java
+++ Preuba1/src/preuba1/Email.java
@@ -10,4 +10,41 @@ package preuba1;
  */
 public class Email {
     
+    private String emisor;
+    private String asunto;
+    private String contenido;
+    private boolean leido;
+    
+    public Email (String emisor, String asunto,String contenido){
+        
+        this.emisor = emisor;
+        this.asunto = asunto;
+        this.contenido = contenido;
+        this.leido = false;
+    }
+    
+    public String getEmisor()    { 
+        return emisor;
+    }
+    public String getAsunto()    { 
+        return asunto;
+    }
+    public String getContenido() { 
+        return contenido;
+    }
+    public boolean isLeido()     { 
+        return leido;
+    }
+    public void marcarLeido(){
+        this.leido = true;
+    }
+    public void imprimir(){
+        System.out.println("De: "+emisor);
+        System.out.println("Asunto:  "+asunto);
+        System.out.println(contenido);
+    }
+    
+    
+    
+    
 }
